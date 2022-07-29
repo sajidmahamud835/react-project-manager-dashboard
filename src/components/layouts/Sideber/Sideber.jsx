7efbf21f8620e8fa3bcdb3aa4f7e79b8d1e5764b
Sajid Mahamud <83598348+sajidmahamud835@users.noreply.github.com>
2022-07-29 11:35:43 +0000

    adding sidebar

--- src/components/layouts/Sideber/Sideber.jsx
+++ src/components/layouts/Sideber/Sideber.jsx
@@ -1,10 +1,29 @@
+import { Dashboard, HeartBroken } from '@mui/icons-material';
 import React from 'react';
+import { ProSidebar, Menu, MenuItem, SubMenu, SidebarContent, SidebarHeader, SidebarFooter } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
 
 const Sideber = () => {
     return (
-        <div>
-            Sidebar
-        </div>
+        <aside>
+            <ProSidebar>
+                <SidebarHeader>
+
+                </SidebarHeader>
+                <SidebarContent>
+                    <Menu iconShape="square">
+                        <MenuItem icon={<Dashboard />}>Dashboard</MenuItem>
+                        <SubMenu title="Components" icon={<HeartBroken />}>
+                            <MenuItem>Component 1</MenuItem>
+                            <MenuItem>Component 2</MenuItem>
+                        </SubMenu>
+                    </Menu>
+                </SidebarContent>
+                <SidebarFooter>
+
+                </SidebarFooter>
+            </ProSidebar>
+        </aside>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './styles/global.css';
 import Header from './layouts/Header/Header';
 import Footer from './layouts/Footer/Footer';
 import Main from './layouts/Main/Main';
+import Sideber from './components/layouts/Sideber/Sideber';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
-    <Main />
+    <app>
+      <Sideber />
+      <Main />
+    </app>
     <Footer />
   </React.StrictMode>
 );
