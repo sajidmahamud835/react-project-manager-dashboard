407ce98fffa23668d712d5e22ca81b0808fd2996
Sajid Mahamud <83598348+sajidmahamud835@users.noreply.github.com>
2022-07-31 13:59:10 +0000

    ui updated

--- src/components/ProjectDetailsCard/ProjectDetailsCard.jsx
+++ src/components/ProjectDetailsCard/ProjectDetailsCard.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Card, CardContent, CardHeader, Typography } from '@mui/material';
+import { Card, CardContent, CardHeader, Chip, Typography } from '@mui/material';
 
 const ProjectDetailsCard = (props) => {
-    const { name, description, image, url } = props.data;
+    const { name, description, category, url } = props.data;
     return (
-        <Card sx={{}}>
+        <Card sx={{ minHeight: 400 }}>
             <CardHeader title={name} />
             <CardContent>
-                <Typography component="p" variant="body1">{description}</Typography>
+                <Chip sx={{ marginTop: 0, marginBottom: 2 }} label={category.name} color={category.color} />
+                <Typography component="p" variant="body1"><strong>Description:</strong> {description}</Typography>
+            </CardContent>
+            <CardContent>
+                <Typography component="p" variant="body2">
+                    <a href={url} target="_blank" rel="noopener noreferrer">
+                        {url}
+                    </a>
+                </Typography>
             </CardContent>
         </Card>
     );
--- src/components/ProjectSummaryCard/ProjectSummaryCard.jsx
+++ src/components/ProjectSummaryCard/ProjectSummaryCard.jsx
@@ -1,14 +1,58 @@
-import { Card, CardContent, CardHeader, Typography } from '@mui/material';
+import { PaymentsTwoTone, Person, SignalWifiStatusbar4Bar } from '@mui/icons-material';
+import { Avatar, Card, CardContent, CardHeader, Chip, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, ownerDocument, Typography } from '@mui/material';
 import React from 'react';
 
 const ProjectSummaryCard = (props) => {
-    const { name, description } = props.data;
+    const { owner, budget, status } = props.data;
     return (
         <div>
             <Card sx={{}}>
                 <CardHeader title="Project Summary" />
                 <CardContent>
-                    <Typography component="p" variant="body1">{description}</Typography>
+                    <List>
+                        <ListItem disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <Person />
+                                </ListItemIcon>
+                                <ListItemText
+                                    primary="Owner"
+                                />
+                                <ListItemText
+                                    secondary={<Chip avatar={<Avatar alt={owner.name} src={owner.avatar} />} label={owner.name} />}
+                                />
+                            </ListItemButton>
+                        </ListItem>
+                        <Divider component="li" />
+                        <ListItem disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <PaymentsTwoTone />
+                                </ListItemIcon>
+                                <ListItemText
+                                    primary="Budget"
+
+                                />
+                                <ListItemText
+                                    secondary={budget}
+                                />
+                            </ListItemButton>
+                        </ListItem>
+                        <Divider component="li" />
+                        <ListItem disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <SignalWifiStatusbar4Bar />
+                                </ListItemIcon>
+                                <ListItemText
+                                    primary="Status"
+                                />
+                                <ListItemText
+                                    secondary={<Chip label={status} />}
+                                />
+                            </ListItemButton>
+                        </ListItem>
+                    </List>
                 </CardContent>
             </Card>
         </div>
