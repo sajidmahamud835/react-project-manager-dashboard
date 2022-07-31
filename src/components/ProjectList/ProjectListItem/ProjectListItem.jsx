3aa36602eff490e11d9ed0bd9205cbd261c4401b
Sajid Mahamud <83598348+sajidmahamud835@users.noreply.github.com>
2022-07-31 11:38:38 +0000

    position changed

--- src/components/ProjectList/ProjectListItem/ProjectListItem.jsx
+++ src/components/ProjectList/ProjectListItem/ProjectListItem.jsx
@@ -6,56 +6,45 @@ const ProjectListItem = (props) => {
     return (
         <div>
             <ListItem alignItems="flex-start">
-                <Grid container spacing={3}>
-                    <Grid item xs={10}>
-                        <ListItemText
-                            primary={
-                                <React.Fragment>
-                                    <Chip sx={{ marginBottom: 1 }} label={category.name} color={category.color} />
+                <ListItemText
+                    primary={
+                        <React.Fragment>
+                            <Chip sx={{ marginBottom: 1 }} label={category.name} color={category.color} />
+                            <Grid container spacing={3}>
+                                <Grid item xs={10}>
                                     <Typography component="h3" variant="h6" color="textPrimary">
                                         {name}
                                     </Typography>
                                     <Typography component="span" variant="body2" color="textDisabled">
                                         {description}
                                     </Typography>
-                                </React.Fragment>
-                            }
-                            secondary={
-                                <React.Fragment>
-                                    <br />
-                                    <Typography sx={{ marginRight: 1 }} component="span" variant="body2" color="textPrimary">
-                                        <Chip
-                                            avatar={<Avatar alt={owner.name} src={owner.avatar} />}
-                                            label={owner.name}
-                                        />
-                                    </Typography>
-                                    <Typography component="span" variant="body2" color="textDisabled">
-                                        {createdAt}
-                                    </Typography>
-                                </React.Fragment>
-                            }
-                        />
-                    </Grid>
-                    <Grid item xs={2}>
-                        <ListItemText
-                            primary={
-                                <React.Fragment>
-                                    <Typography component="span" variant="h3" color="textDisabled">
+                                </Grid>
+                                <Grid item xs={2}>
+                                    <Typography component="span" variant="h4" color="textDisabled">
                                         {budget}
                                     </Typography>
-                                </React.Fragment>
-
-                            }
-                            secondary={
-                                <React.Fragment>
                                     <Chip sx={{ marginTop: 3 }} label={status} />
-                                </React.Fragment>
-                            }
-                        />
-                    </Grid>
-                </Grid>
+                                </Grid>
+                            </Grid>
+                        </React.Fragment>
+                    }
+                    secondary={
+                        <React.Fragment>
+                            <br />
+                            <Typography sx={{ marginRight: 1 }} component="span" variant="body2" color="textPrimary">
+                                <Chip
+                                    avatar={<Avatar alt={owner.name} src={owner.avatar} />}
+                                    label={owner.name}
+                                />
+                            </Typography>
+                            <Typography component="span" variant="body2" color="textDisabled">
+                                {createdAt}
+                            </Typography>
+                        </React.Fragment>
+                    }
+                />
             </ListItem>
-        </div>
+        </div >
     );
 };
 
